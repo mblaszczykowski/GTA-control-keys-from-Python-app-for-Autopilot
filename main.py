c6becf8fff7240704881b34fc2b6ac8e32714e69
mblaszczykowski <79908334+mblaszczykowski@users.noreply.github.com>
2022-07-28 18:13:33 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,113 @@
+import ctypes
+import time
+
+SendInput = ctypes.windll.user32.SendInput
+
+W = 0x11
+A = 0x1E
+S = 0x1F
+D = 0x20
+
+J = 0x24
+K = 0x25
+I = 0x17
+L = 0x26
+
+G = 0x22
+SPACE = 0x39
+
+# C struct redefinitions 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Actuals Functions
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def addThrust():
+    print("adding thrust")
+    PressKey(W)
+
+def releaseThrust():
+    print("releasing thrust")
+    ReleaseKey(W)
+
+
+if __name__ == '__main__':
+    print("\nAUTOPILOT GTA V - JET\n")
+    # waiting 5 sec to be ready to start
+    for i in range(5):
+        print(5-i)
+        time.sleep(1)
+
+    # start
+
+    # beep
+    '''
+    import winsound
+    frequency = 2500  # Set Frequency To 2500 Hertz
+    duration = 1000  # Set Duration To 1000 ms == 1 second
+    winsound.Beep(frequency, duration)
+    '''
+
+    addThrust()
+    time.sleep(6)
+
+    for i in range(17):
+        PressKey(I)
+        time.sleep(0.4)
+        ReleaseKey(I)
+
+        if i>6:
+            time.sleep(3)
+
+        PressKey(K)
+        time.sleep(0.2)
+        ReleaseKey(K)
+
+        if i>8:
+            PressKey(J)
+            time.sleep(0.2)
+            ReleaseKey(J)
+
+
+    releaseThrust()
